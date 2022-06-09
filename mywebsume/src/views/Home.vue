4cdc5f5d64a63806f610d57ea123fef8e3331d1a
jonric <jonric.manisan@lsu.edu.ph>
2022-06-09 19:30:40 +0800

    vue

---
+++ mywebsume/src/views/Home.vue
@@ -0,0 +1,313 @@
+<template>
+  <v-app
+    id="home"
+    :style="{ background: $vuetify.theme.themes.dark.background }"
+  >
+    <nav-bar />
+    <v-container fluid>
+      <v-row>
+        <v-col cols="6">
+          <v-img src="9.png" contain max-height="900"></v-img>
+        </v-col>
+        <v-col cols="6">
+          <h5 class="red--text text--darken-4 top">Hello Im Jonric Manisan</h5>
+          <h1 class="white--text">My Websume in ADWEB</h1>
+          <p class="grey--text">
+            I'm Jonric Manisan Bachelor of Science in Information Technology  <br />
+            “It's not about perfect. It's about effort.” 
+            <br />
+            Final Exam
+          </p>
+          <v-btn tile color="#A7121D" dark> about me </v-btn>
+        </v-col>
+        <v-col cols="12" class="padd">
+          <div class="first" id="project">
+            <v-row>
+              <v-col cols="12">
+                <div class="child bgColor1">
+                  <v-icon color="#A7121D" x-large class="ml-3"
+                    >mdi-music-circle</v-icon
+                  >
+                  <h3 class="white--text ml-3 mt-4">Music Lover</h3>
+                  <p class="grey--text ml-3 mt-6">
+                   Paraluman, Southboys, etc <br />PopSong, LoveSong, PartySong etc
+                  </p>
+                
+                </div>
+                <div class="child bgColor2">
+                  <v-icon color="" x-large class="ml-3" dark
+                    >mdi-gamepad-round-left</v-icon
+                  >
+                  <h3 class="white--text ml-3 mt-4">Gamer</h3>
+                  <p class="grey--text ml-3 mt-6">
+                    ML,CODM,COC <br />DOTA2,VALORANT
+                  </p>
+                 
+                </div>
+                <div class="child bgColor1">
+                  <v-icon color="#A7121D" x-large class="ml-3"
+                    >mdi-monitor</v-icon
+                  >
+                  <h3 class="white--text ml-3 mt-4">Programmer</h3>
+                  <p class="grey--text ml-3 mt-6">
+                    React, Vue, Laravel, <br />c++, python, 
+                  </p>
+                  
+                </div>
+              </v-col>
+              <v-col cols="12" class="mt-10">
+                <div class="child1">
+                  <h1 class="red--text text--darken-4 mt-4 number">My Goal</h1>
+                  <h3 class="white--text mt-4">PLAN</h3>
+                </div>
+                <div class="child2 mRight">
+                  <v-row>
+                    <v-col cols="12" class="childcol">
+                      <div class="child2 mButton padding bgColor1">
+                        <h1 class="red--text text--darken-4">Graduate</h1>
+                        <p class="grey--text">in Lasalle University</p>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" class="childcol">
+                      <div class="child2 padding bgColor1">
+                        <h1 class="red--text text--darken-4">Find a Job</h1>
+                        <p class="grey--text">Goods na Company</p>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </div>
+                <div class="child2">
+                  <v-row>
+                    <v-col cols="12" class="childcol">
+                      <div class="child2 mButton padding bgColor1">
+                        <h1 class="red--text text--darken-4">Earn Money</h1>
+                        <p class="grey--text">para naay pang laag</p>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" class="childcol">
+                      <div class="child2 padding bgColor1">
+                        <h1 class="red--text text--darken-4">Adventure</h1>
+                        <p class="grey--text">with Fam, Uyab & Friends</p>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </div>
+              </v-col>
+            </v-row>
+          </div>
+        </v-col>
+        <v-col md="3" offset-md="3" id="about">
+          <h4 class="white--text">The Place I want to Explore</h4>
+          <p class="grey--text">TRAVEL</p>
+        </v-col>
+        <v-col md="3" class="text-end">
+        </v-col>
+        <v-col md="3" offset-md="3">
+          <v-card
+            class="pa-2 py-12"
+            outlined
+            tile
+            height="250px"
+            color="#1E1E1E"
+          >
+            <v-img src="8.jpg" contain max-height=""></v-img>
+          </v-card>
+        </v-col>
+        <v-col md="3">
+          <v-card
+            class="pa-2 py-12"
+            outlined
+            tile
+            height="250px"
+            color="#1E1E1E"
+          >
+            <v-img src="11.jpg" contain max-height=""></v-img>
+          </v-card>
+        </v-col>
+        <v-col md="3" offset-md="3">
+          <v-btn color="white" dark text class="ml-n4">Maldives</v-btn
+          ><br />
+
+        </v-col>
+        <v-col md="3">
+          <v-btn color="white" dark text class="ml-n4">Dubai </v-btn><br />
+
+  
+        </v-col>
+        <v-col md="3" offset-md="3">
+          <v-card
+            class="pa-2 py-12"
+            outlined
+            tile
+            height="250px"
+            color="#1E1E1E"
+          >
+            <v-img src="10.jpg" contain max-height=""></v-img>
+          </v-card>
+        </v-col>
+        <v-col md="3">
+          <v-card
+            class="pa-2 py-12"
+            outlined
+            tile
+            height="250px"
+            color="#1E1E1E"
+          >
+            <v-img src="12.jpg" contain max-height=""></v-img>
+          </v-card>
+        </v-col>
+        <v-col md="3" offset-md="3">
+          <v-btn color="white" dark text class="ml-n4"> Canada </v-btn
+          ><br />
+
+          
+        </v-col>
+        <v-col md="3">
+          <v-btn color="white" dark text class="ml-n4"> Italy </v-btn><br />
+          >
+        </v-col>
+        <v-col cols="12" class="padd topInverse">
+          <div class="second">
+            <div class="secondchild1" id="contact">
+              <v-row>
+                <v-col cols="7">
+                  <h1 class="white--text">
+                    Just call me<br />09052164858
+                  </h1>
+                  <p class="grey--text">
+                    GLOBE <br />
+                  </p>
+                </v-col>
+                <v-col cols="5">
+                  <v-btn tile color="#A7121D" dark class="mt-16">
+                    Contact
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </div>
+          </div>
+          <v-toolbar class="topTolbar" color="#111111" dark flat>
+            <div
+              style="
+                position: absolute;
+                margin-left: auto;
+                margin-right: auto;
+                left: 0;
+                right: 0;
+                text-align: center;
+              "
+            >
+              <v-btn text>Home</v-btn>
+              <v-btn text>Project</v-btn>
+              <v-btn text>about</v-btn>
+              <v-btn text> contact</v-btn>
+            </div>
+          </v-toolbar>
+        </v-col>
+      </v-row>
+    </v-container>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import NavBar from "../components/NavBar.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+  name: "Home",
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+  }),
+  components: {
+    NavBar,
+    Footer,
+  },
+};
+</script>
+<style scoped>
+.top {
+  margin-top: 180px;
+}
+.topInverse {
+  margin-top: -250px;
+}
+.topTolbar {
+  margin-top: 100px;
+  text-align: center;
+}
+.first {
+  width: 100%;
+  height: 610px;
+  background: linear-gradient(
+    to right,
+    #181818,
+    #181818 50%,
+    #111111 50%,
+    #111111 50%
+  );
+  text-align: center;
+  padding: 2rem 2rem;
+}
+.second {
+  width: 100%;
+  height: 400px;
+  background: #181818;
+  text-align: center;
+  padding: 2rem 2rem;
+}
+.secondchild1 {
+  display: inline-block;
+  background-color: #1e1e1e;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: left;
+  margin-top: 250px;
+}
+.child {
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: left;
+  margin-right: 8px;
+}
+.bgColor1 {
+  background-color: #1e1e1e;
+}
+.bgColor2 {
+  background-color: #ce1d2a;
+}
+
+.child1 {
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  margin-right: 5px;
+  width: 240px;
+}
+.child2 {
+  display: inline-block;
+  width: 245px;
+  vertical-align: middle;
+}
+.mRight {
+  margin-right: 8px;
+}
+.mButton {
+  margin-bottom: 8px;
+}
+.padding {
+  padding: 8px 0;
+}
+
+.col-12.padd {
+  padding: 12px 0 !important;
+}
+.col-12.childcol {
+  padding: 0 !important;
+}
+h1.number {
+  font-size: 50px;
+  font-weight: bold;
+}
+</style>
